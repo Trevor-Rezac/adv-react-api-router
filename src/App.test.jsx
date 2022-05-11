65af8cee0d360bb8a184507153b1a9eed2dd2fe1
TrevorRezac <trev.rezac@gmail.com>
2022-05-10 17:15:02 -0700

    passing test for character detail page

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 import App from './App';
+import CharacterDetail from './views/Characters/Detail';
 
 describe('App.jsx tests', () => {
   it('should render a list of characters on load', async () => {
@@ -27,5 +28,21 @@ describe('App.jsx tests', () => {
     //tests that 20 character images containing alt text appears on the page
     const characterImages = await screen.findAllByAltText(/image of/i);
     expect(characterImages.length).toEqual(20);
+  })
+
+  it('should render character details when a user clicks a list item', async () => {
+    render(
+      //pass the characters route an initial character id of 2
+      <MemoryRouter initialEntries={['/characters/2']}>
+        <Route exact path='/characters/:id'>
+          <CharacterDetail />
+        </Route>
+      </MemoryRouter>
+    )
+
+    //tests that there is only one character header with the text "Morty Smith"
+    const characterHeader = await screen.findAllByRole('heading', { level: 3});
+    expect(characterHeader.length).toEqual(1);
+    await screen.findByText('Morty Smith')
   })
 })
